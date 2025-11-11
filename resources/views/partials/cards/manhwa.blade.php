6ecca7ae4b16d50caa1ac7017cb6fe5b69009f6f
KateKateline <rizkyramadhan12098@gmail.com>
2025-11-11 23:54:42 +0700

    slug cards aditional

--- resources/views/partials/cards/manhwa.blade.php
+++ resources/views/partials/cards/manhwa.blade.php
@@ -1,11 +1,12 @@
 @forelse($manhwa as $item)
-    <div class="bg-[#161b22] rounded-xl p-3 shadow hover:shadow-lg transition">
+    <a href="{{ route('comic.show', $item->slug) }}"
+    class="block bg-[#161b22] rounded-xl p-3 shadow hover:shadow-lg hover:scale-[1.02] transition"> 
         <img src="{{ asset('storage/'.$item->cover_image) }}" 
              alt="{{ $item->title }}" 
              class="rounded-lg w-full h-52 object-cover mb-3">
         <h3 class="text-lg font-semibold truncate">{{ $item->title }}</h3>
         <p class="text-sm text-gray-400">{{ $item->author ?? 'Unknown' }}</p>
-    </div>
+    </a>
 @empty
     <p class="text-gray-400">Belum ada manhwa yang diunggah.</p>
 @endforelse
